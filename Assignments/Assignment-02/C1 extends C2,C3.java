91e7fbce791d2a3813b5b07321b690b4c824d7d4
pallelokesh25csm-jpg <pallelokesh.25.csm@anits.edu.in>
2026-08-29 22:10:43 +0530

    Add files via upload

---
+++ Assignments/Assignment-02/C1 extends C2,C3.java
@@ -0,0 +1,15 @@
+class C2 {
+    void displayC2() {
+        System.out.println("This is C2");
+    }
+}
+
+class C3 {
+    void displayC3() {
+        System.out.println("This is C3");
+    }
+}
+
+//  Not allowed
+//class C1 extends C2, C3 {
+//}
