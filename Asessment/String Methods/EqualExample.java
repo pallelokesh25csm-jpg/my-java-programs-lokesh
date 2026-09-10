c136a94f0b03889de557bd8c1ba8769688c49867
pallelokesh25csm-jpg <pallelokesh.25.csm@anits.edu.in>
2026-09-10 19:12:44 +0530

    Add files via upload

---
+++ Asessment/String Methods/EqualExample.java
@@ -0,0 +1,10 @@
+public class EqualExample {
+    public static void main(String[] args) {
+        String str1 = "Lucky";
+        String str2 = "Lucky";
+        if (str1.equals(str2))
+            System.out.println("Strings are same");
+        else
+            System.out.println("Strings are not same");
+    }
+}
---
+++ Asessment/String Methods/SubstringExample.java
@@ -0,0 +1,6 @@
+public class SubstringExample {
+    public static void main(String[] args) {
+        String str ="Programming";
+System.out.println(str.substring(0,7));
+    }
+}
