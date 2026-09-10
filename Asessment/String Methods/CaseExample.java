305abc0e8d82a8eeb876eed08bba2bc12a2e97f1
pallelokesh25csm-jpg <pallelokesh.25.csm@anits.edu.in>
2026-09-10 18:57:04 +0530

    Add files via upload

---
+++ Asessment/String Methods/CaseExample.java
@@ -0,0 +1,7 @@
+public class CaseExample {
+   public static void main(String[] args) {
+    String str = "Lucky Palle";
+    System.out.println(str.toUpperCase());
+    System.out.println(str.toLowerCase());
+   } 
+}
