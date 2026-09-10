1c778510606bd04487f6f08b5427b02a0ade01d0
pallelokesh25csm-jpg <pallelokesh.25.csm@anits.edu.in>
2026-09-10 22:14:15 +0530

    Add files via upload

---
+++ Asessment/String Methods/IndexExample.java
@@ -0,0 +1,7 @@
+public class IndexExample {
+    public static void main(String[] args) {
+        String str = "lokesh";
+        
+        System.out.println("position= " + str.indexOf('h'));
+    }
+}
