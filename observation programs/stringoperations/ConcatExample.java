aacbf4f0ab9fcb3854958507c983d906ec33f35f
pallelokesh25csm-jpg <pallelokesh.25.csm@anits.edu.in>
2026-09-16 09:17:21 +0530

    Add files via upload

---
+++ observation programs/stringoperations/ConcatExample.java
@@ -0,0 +1,9 @@
+public class ConcatExample {
+   public static void main(String[] args) {
+    String s1 = "Lokesh";
+    String s2 = "Palle";
+
+    String s3 = s1.concat(s2);
+System.out.println("s3");
+   }
+}
---
+++ observation programs/stringoperations/ContainsExample.java
@@ -0,0 +1,12 @@
+public class ContainsExample {
+    public static void main(String[] args) {
+        String str = "java programming";
+        if (str.contains("java")) {
+            System.out.println("java is present");
+            else{
+        
+                System.out.println("java is not present");
+            
+        }
+    }
+}
